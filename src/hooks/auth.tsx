e7aeee7a8226949d43b95678e830baf9d714bb76
andersonfrfilho <andersonfrfilho@gmail.com>
2020-06-14 03:15:33 -0300

    feature:finally-project

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -8,20 +8,27 @@ import React, {
 import AsyncStorage from '@react-native-community/async-storage';
 import api from '../services/api';
 
+interface User {
+  id: string;
+  name: string;
+  email: string;
+  avatar_url: string;
+}
 interface SignInCredentials {
   email: string;
   password: string;
 }
 
 interface AuthContextData {
-  user: object;
+  user: User;
   signIn(credentials: SignInCredentials): Promise<void>;
   signOut(): void;
   loading: boolean;
+  updateUser(user: User): Promise<void>;
 }
 interface AuthState {
   token: string;
-  user: object;
+  user: User;
 }
 const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
@@ -35,6 +42,7 @@ const AuthProvider: React.FC = ({ children }) => {
         '@GoBarber:user',
       ]);
       if (token[1] && user[1]) {
+        api.defaults.headers.authorization = `Bearer ${token[1]}`;
         setData({ token: token[1], user: JSON.parse(user[1]) });
       }
       setLoading(false);
@@ -52,6 +60,7 @@ const AuthProvider: React.FC = ({ children }) => {
       ['@GoBarber:token', token],
       ['@GoBarber:user', JSON.stringify(user)],
     ]);
+    api.defaults.headers.authorization = `Bearer ${token}`;
     setData({
       token,
       user,
@@ -61,8 +70,20 @@ const AuthProvider: React.FC = ({ children }) => {
     await AsyncStorage.multiRemove(['@GoBarber:token', '@GoBarber:user']);
     setData({} as AuthState);
   }, []);
+  const updateUser = useCallback(
+    async (user: User) => {
+      await AsyncStorage.setItem('@GoBarber:user', JSON.stringify(user));
+      setData({
+        token: data.token,
+        user,
+      });
+    },
+    [setData, data.token],
+  );
   return (
-    <AuthContext.Provider value={{ user: data.user, signIn, signOut, loading }}>
+    <AuthContext.Provider
+      value={{ user: data.user, signIn, signOut, loading, updateUser }}
+    >
       {children}
     </AuthContext.Provider>
   );
